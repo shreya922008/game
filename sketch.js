ce8b59bafbd78fa46c71277cc0f22cf090880f56
shreya922008 <72599533+shreya922008@users.noreply.github.com>
2020-11-19 11:18:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var car,wall;
+var speed,weight
+
+
+function setup(){
+
+createCanvas(1600, 400);
+
+speed=random(22,25)
+weight=random(400,1500);
+
+car=createSprite(45, 200,45,45);
+car.velocityX = speed;
+
+
+
+wall=createSprite(1200,200,60,height/2);
+wall.shapeColor="lightblue";
+
+}
+
+
+function draw(){
+background (0);
+
+if(wall.x-car.x<(car.width+wall.width)/2)
+{
+  car.velocityX=0;
+  var deformation=0.5 * weight * speed * speed/22509;
+  if(deformation>180);
+  {
+    car.shapeColor="blue"
+  }
+  if(deformation<180 && deformation>100)
+  {
+    car.shapeColor="orange"
+  }
+  if(deformation<100)
+  {
+    car.shapeColor="pink "
+  }
+}
+
+drawSprites()
+
+}
